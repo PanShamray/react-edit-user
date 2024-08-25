b9213263c010e1843fda72e6a0577722d5e05271
Vladislav Shamray <127450674+PanShamray@users.noreply.github.com>
2024-08-25 18:49:14 +0300

    Implemented the ability to navigate between pages using React-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import './App.scss';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import EditUser from "./pages/EditUser/EditUser"
+import Users from "./pages/Users/Users";
 
 function App() {
   return (
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <Router>
+      <div>
+        <button>
+          <Link to="/edit-user">Edit User</Link>
+        </button>
+        <button>
+          <Link to="/">Users</Link>
+        </button>
+
+        <Routes>
+          <Route path="/" element={<Users />} />
+          <Route path="/edit-user" element={<EditUser />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
