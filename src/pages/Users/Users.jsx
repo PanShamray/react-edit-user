107b5356562a1e1f730483bc3012f27ae415ced6
Vladislav Shamray <127450674+PanShamray@users.noreply.github.com>
2024-09-12 14:11:53 +0300

    fixed scrollbar and select-options

--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -1,9 +1,9 @@
-import React, {useState} from 'react';
-import Select from "react-select";
+import React, { useState } from "react";
+import Select, { components } from "react-select";
 import { users, departments, statuses, countries } from "../../data.js";
 import "./style.scss";
-import TrashButton from "../../components/TrashButton.jsx"
-import AddUserModal from '../../components/AddUserModal/AddUserModal.jsx';
+import TrashButton from "../../components/TrashButton.jsx";
+import AddUserModal from "../../components/AddUserModal/AddUserModal.jsx";
 
 function Users() {
   const [usersList, setUsersList] = useState(users);
@@ -15,6 +15,48 @@ function Users() {
 
   const isFiltersActive = selectedDepartments.length >= 3;
 
+  const CheckboxOption = ({ children, ...props }) => {
+    return (
+      <components.Option {...props}>
+        <input
+          className="checkbox"
+          type="checkbox"
+          checked={props.isSelected}
+          onChange={() => null}
+          style={{
+            width: 24,
+            height: 24,
+            border: "none",
+            borderColor: "black",
+            borderRadius: 0,
+            verticalAlign: "middle",
+            marginRight: 12,
+            marginTop: 1,
+            marginBottom: 1,
+            marginLeft:0
+          }}
+        />
+        <span className="custom-checkbox"></span>
+        {children}
+      </components.Option>
+    );
+  };
+
+  const ValueContainer = ({ children, getValue, ...props }) => {
+    const selectedOptions = getValue(); // Отримуємо вибрані елементи
+    const hasValue = selectedOptions.length > 0; // Перевіряємо чи є вибрані елементи
+
+    return (
+      <components.ValueContainer {...props}>
+        {hasValue ? (
+          <div>{`Selected (${selectedOptions.length})`}</div>
+        ) : (
+          children
+        )}
+      </components.ValueContainer>
+    );
+  };
+  
   const handleDepartmentChange = (selectedOptions) => {
     setSelectedDepartments(selectedOptions);
 
@@ -80,7 +122,14 @@ function Users() {
                 }))}
                 value={selectedDepartments}
                 onChange={handleDepartmentChange}
-                placeholder="Department"
+                placeholder="Select departments"
+                closeMenuOnSelect={false}
+                hideSelectedOptions={false}
+                components={{
+                  Option: CheckboxOption,
+                  ValueContainer: (props) => <ValueContainer {...props} />
+                }}
+                
               />
               <Select
                 options={countries.map((country) => ({
@@ -126,22 +175,20 @@ function Users() {
               <table>
                 <tbody>
                   {filteredUsers.map((user) => (
-                    <>
-                      <tr key={user.name}>
-                        <td className="tdName">{user.name}</td>
-                        <td className="tdDepartment">{user.department.name}</td>
-                        <td className="tdCountry">{user.country.name}</td>
-                        <td className="tdStatus">{user.status.name}</td>
-                        <td>
-                          <button
-                            onClick={() => handleUserDelete(user.name)}
-                            className="trashButton"
-                          >
-                            <TrashButton />
-                          </button>
-                        </td>
-                      </tr>
-                    </>
+                    <tr key={user.name}>
+                      <td className="tdName">{user.name}</td>
+                      <td className="tdDepartment">{user.department.name}</td>
+                      <td className="tdCountry">{user.country.name}</td>
+                      <td className="tdStatus">{user.status.name}</td>
+                      <td>
+                        <button
+                          onClick={() => handleUserDelete(user.name)}
+                          className="trashButton"
+                        >
+                          <TrashButton />
+                        </button>
+                      </td>
+                    </tr>
                   ))}
                 </tbody>
               </table>
